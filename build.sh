9df48a686c1e2a1c7c4aa702b8fafd117fe198f9
Toshiyuki Sakai <totoyk104@gmail.com>
2025-07-08 10:27:17 +0900

    added sample scripts

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+log_file="$(dirname "$0")/build.log"
+echo "$(date '+%Y-%m-%d %H:%M:%S') build" >> "$log_file"
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+log_file="$(dirname "$0")/run.log"
+echo "$(date '+%Y-%m-%d %H:%M:%S') run" >> "$log_file"
